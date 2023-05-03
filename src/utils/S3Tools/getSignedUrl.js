aeee1c58d560478ce77b524c5c5b462de2b867e4
Lucas Lopes <lopes.sp.lucas@gmail.com>
2023-05-03 07:23:39 -0300

    include environment connections params to AWS S3

--- src/utils/S3Tools/getSignedUrl.js
+++ src/utils/S3Tools/getSignedUrl.js
@@ -4,11 +4,17 @@ import { config } from 'dotenv';
 config();
 
 const getSignedUrl = async (key) => {
-    const s3 = new AWS.S3();
+    const s3 = new AWS.S3({
+        accessKeyId: process.env.AWS_S3_UPLOAD_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_S3_UPLOAD_SECRET_ACCESS_KEY,
+        signatureVersion: 'v4',
+        region: process.env.BUCKET_REGION,
+    });
 
     const params = {
         Bucket: process.env.BUCKET_NAME,
         Key: key,
+        Expires: 60 * 60
     };
 
     const res = await s3.getSignedUrlPromise('getObject', params);
--- src/utils/S3Tools/putObject.js
+++ src/utils/S3Tools/putObject.js
@@ -4,17 +4,22 @@ import { config } from 'dotenv';
 config();
 
 const putObject = async (folder, name, bodyBuffer) => {
-    const s3 = new AWS.S3();
+  const s3 = new AWS.S3({
+    accessKeyId: process.env.AWS_S3_UPLOAD_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_S3_UPLOAD_SECRET_ACCESS_KEY,
+    signatureVersion: 'v4',
+    region: process.env.BUCKET_REGION,
+  });
 
-    const res = await s3.upload({
-      Key: `${folder}/${name}`,
-      Bucket: process.env.BUCKET_NAME,
-      Body: bodyBuffer,
-      ACL: 'private',
-      ContentType: 'application/pdf'
-    }).promise();
+  const res = await s3.upload({
+    Key: `${folder}/${name}`,
+    Bucket: process.env.BUCKET_NAME,
+    Body: bodyBuffer,
+    ACL: 'private',
+    ContentType: 'application/pdf'
+  }).promise();
 
-    return res;
+  return res;
 }
 
 export default putObject;
